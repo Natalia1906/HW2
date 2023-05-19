d1fdf3b3c8c5ab15ded65c25595fa6b8e4c468b9
Natalia1906 <siverina.ze2006@gmail.com>
2023-05-19 18:20:36 +0600

    updaty

---
+++ hw/17.05.py
@@ -0,0 +1,32 @@
+# 1 задача
+a = int(input('введите значение a:'))
+b = int(input('введите значение b:'))
+c = int(input('введите значение c:'))
+if a + b > c and a + c > b and b + c > a :
+    print ('True')
+else :
+    print('False')
+
+# 2 задача
+a = int(input('Введите число: '))
+if a % 2 == 0 :
+    print('True')
+else :
+    print('False')
+
+# 3 задача
+a = int(input('введите значение a:'))
+b = int(input('введите значение b:'))
+c = int(input('введите значение c:'))
+if a + b > c :
+    print('True')
+else :
+    print('False')
+
+# 4 задача
+a = int(input('введите значение a:'))
+b = int(input('введите значение b:'))
+if a > b :
+    print('True')
+else :
+    print('False')
