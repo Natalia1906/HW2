cdeba96871b8b56a83b958daa3f7e9db9ab958b1
Natalia1906 <siverina.ze2006@gmail.com>
2023-06-22 10:26:16 +0600

    update

--- prac_19.06.py
+++ prac_19.06.py
@@ -1,25 +1,59 @@
+import threading
+import multiprocessing
 import requests
 import json
 import time
 
-def save_todo():
-    res = requests.get("https://jsonplaceholder.typicode.com/todos/1")
+def save_1_todo(num):
+    res = requests.get(f"https://jsonplaceholder.typicode.com/todos/{num}")
     todo = res.json()
-    with open("temp/todo.json", "w") as file:
+    with open(f"temp/todo{num}.json", "w") as file:
         json.dump(todo, file)
 
-save_todo()
-
-def save10_todo(func):
+@decorator_time_measure
+def save_10_todo_sync():
     for i in range(1, 10+1):
+        save_1_todo(i)
 
-def decorator(func):
+@decorator_time_measure
+def save_10_todo_threading():
+    thread_list = []
+    for i in range(1, 10+1):
+        thread_list.append(threading.Thread(target=save_1_todo, args=(i,),kwargs={}))
+    for thread in thread_list:
+        thread.start()
+    for thread in thread_list():
+        thread.join()
+    for i in range(1, 10+1):
+        save_1_todo(i)
+
+@decorator_time_measure
+def save_10_todo_multiprocessing():
+    process_list = []
+    for i in range(1, 10+1):
+        process_list.append(multiprocessing.Process(target=save_1_todo, args=(i,),kwargs={}))
+    for process in process_list:
+        process.start()
+    for process in process_list():
+        process.join()
+
+
+
+def decorator_time_measure(func):
     def wrapper(*args, **kwargs):
-        start = time.perf_counter()
-        res = func(*args, **kwargs)
-        stop = time.perf_counter()
-        print(stop - start)
-        return res
+        time_start = time.perf_counter()
+
+        result = func(*args, **kwargs)
+
+        print("elapsed_time(s): ", round(time.perf_counter() - time_start, 6))
+        return result
+
     return wrapper
 
+
+if __name__ == "__main__":
+    #save_10_todo_sync()
+    #save_1_todo(10)
+    #save_10_todo_threading()
+    #save_10_todo_multiprocessing()
 
